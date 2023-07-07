5df37e5336391d6f1a7cd22182296d73974e56fd
TsotneBasili <137988770+TsotneBasili@users.noreply.github.com>
2023-07-07 15:56:50 +0400

    Add files via upload

---
+++ meotxe_davaleba.py
@@ -0,0 +1,29 @@
+class Student:
+    university = "Harward"
+
+    def __init__(self, name, grade, age):
+        self.name = name
+        self.grade = grade
+        self.age = age
+
+    def __str__(self):
+        return f"Name: [{self.name}], Age: [{self.age}], Grade: [{self.grade}]"
+
+    @property
+    def is_passing(self):
+        if self.grade > 60:
+            return True
+        else:
+            return False
+
+    def increase_grade(self, add_grade):
+        self.grade += add_grade
+        return self.grade
+
+
+# bondo = Student("Bondo",99, 23)
+#
+# bondo.increase_grade(1)
+# print(bondo.is_passing)
+# print(bondo.grade)
+# print(str(bondo))
